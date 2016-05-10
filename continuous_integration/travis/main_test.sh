46702dd7b12c7e3be346c8a8f34a9b296013582c
Pierre Raybaut <pierre.raybaut@gmail.com>
2016-05-10 12:27:54 +0200

    Update main_test.sh

--- continuous_integration/travis/main_test.sh
+++ continuous_integration/travis/main_test.sh
@@ -5,6 +5,6 @@ set -ex
 # Tell formlayout we're testing our widgets in Travis
 export TEST_CI_WIDGETS=True
 
-python examples/simple.py
-python examples/advanced.py
 python formlayout.py
+python ./examples/simple.py
+python ./examples/advanced.py
