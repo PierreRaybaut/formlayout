8b3c56f7adb432c84586e033b202959d4d0aee40
Pierre Raybaut <pierre.raybaut@gmail.com>
2016-05-10 18:06:47 +0200

    build_dist_test.sh: commenting problematic parts

--- continuous_integration/travis/build_dist_test.sh
+++ continuous_integration/travis/build_dist_test.sh
@@ -7,12 +7,17 @@ pip --version
 
 cd $HOME/build/PierreRaybaut/formlayout
 
-# Checkout the right branch
-if [ $TRAVIS_PULL_REQUEST != "false" ] ; then
-    git checkout travis_pr_$TRAVIS_PULL_REQUEST
-else
-    git checkout master
-fi
+# Is the following really necessary?
+## Checkout the right branch
+#if [ $TRAVIS_PULL_REQUEST != "false" ] ; then
+#    git checkout travis_pr_$TRAVIS_PULL_REQUEST
+#else
+#    git checkout master
+#fi
 
-python setup.py sdist bdist_wheel --universal
+# Wheel build is causing a segfault when using PyQt4!?
+## Building universal wheel package
+# python setup.py sdist bdist_wheel --universal
+
+# Building Python source package
 python setup.py build sdist
